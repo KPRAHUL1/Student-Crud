53efd3da38c6fd8e9affadb564fc46eb34a7e562
Rahul K <rahulsachin07314@gmail.com>
2025-07-20 17:31:35 +0530

    create get and add student details

--- src/app/store.js
+++ src/app/store.js
@@ -1,7 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
+import studentReducer from '../todo/todoSlice'
 
 export const store = configureStore({
   reducer: {
-    
-  }
+    student: studentReducer,
+  },
 })
+
